829f84e48274fc5d9124c922dfd8032438215aed
German Sizov <germansiz0v@yandex.ru>
2022-04-16 00:00:23 +0300

    Edit frontend:footer and single-card

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,10 +1,18 @@
 import styles from './style.module.css'
+import { useContext } from 'react'
 import { Container, LinkComponent } from '../index'
+import { UserContext, AuthContext } from '../../contexts'
 
 const Footer = () => {
+  const authContext = useContext(AuthContext)
+  const userContext = useContext(UserContext)
   return <footer className={styles.footer}>
       <Container className={styles.footer__container}>
-        <LinkComponent href='#' title='Продуктовый помощник' className={styles.footer__brand} />
+        {<LinkComponent
+          href={authContext ? `/user/${userContext.id}` : '/signin'}
+          title='Твои рецепты'
+          className={styles.footer__brand} 
+        />}
       </Container>
   </footer>
 }
